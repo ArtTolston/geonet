96007313f82148336ae6411645a32411c8947d72
Artem <tolstonozhenko.as@frtk.com>
2021-04-28 00:51:52 +0300

    Add authorization and check

--- db.py
+++ db.py
@@ -1,15 +1,42 @@
 import psycopg2
 import click
-from flask import current_app, g
-from flask.cli import with_appcontext
 
-def get_db():
-	if 'db' not in g:
-		g.db = psycopg2.connect(dbname=current_app.config['DATABASE'], user=current_app.config['USER'])
-	return g.db
+import psycopg2.extras
 
 
-def init_db():
-    db = psycopg2.connect(user='udk2018', dbname='geonet')
-    with open('schema.sql', 'r') as f:
-        db.cursor().execute(f.read())
+class GeonetDB:
+	def __init__(self, db):
+		self.__db = db
+		self.__cursor = db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+		
+
+	def addUser(self, login, hash):
+		try:
+			self.__cursor.execute('INSERT INTO users (login, passwd) VALUES (%s, %s);', (login, hash))
+			print('user seccusfully added')
+			self.__db.commit()
+		except psycopg2.Error as e:
+			print(e.pgerror)
+
+	def isFreeLogin(self, login):
+		try:
+			self.__cursor.execute('SELECT count(*) as cnt FROM users WHERE login = %s', (login,))
+			amount = self.__cursor.fetchone()
+			print(amount)
+			if amount[0] == 0:
+				return True
+			else:
+				return False
+		except psycopg2.Error as e:
+			print(e.pgerror)
+			return False
+
+	def getUserByLogin(self, login):
+		self.__cursor.execute('SELECT * FROM users WHERE login = %s', (login,))
+		user = self.__cursor.fetchone()
+		return user
+
+
+	def executeScript(self, script):
+		self.__cursor.execute(script)
+		self.__db.commit()
--- geonet.py
+++ geonet.py
@@ -1,8 +1,12 @@
 import os
-import psycopg2.extras
-from flask import Flask, redirect, url_for, render_template, request, session, g
+from db import GeonetDB 
+import psycopg2 
+from login import UserLogin
+from flask_login import LoginManager, login_user, login_required
+from flask import Flask, redirect, url_for, render_template, request, session, g, current_app
 from werkzeug.security import generate_password_hash, check_password_hash
-from db import get_db
+from flask.cli import with_appcontext
+
 
 
 
@@ -10,34 +14,56 @@ app = Flask(__name__)
 enviroment_configuration = os.environ['FLASK_CONFIGURATION_SETUP']
 app.config.from_object(enviroment_configuration)
 
+login_manager = LoginManager(app)
+
 
 @app.route('/')
 def index():
    return render_template('base.html')
 
+
 @app.route('/map')
+@login_required
 def map():
 	city='moscow'
 	return render_template('map.html', city=city)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if request.method == 'POST':
+		user = geodb.getUserByLogin(request.form['login'])
+		if user and check_password_hash(user['passwd'], request.form['passwd']):
+			userLogin = UserLogin().create(user)
+			login_user(userLogin)
+			return redirect(url_for('index'))
 	return render_template('login.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 	if request.method == 'POST':
 		if len(request.form['login']) >= 0 and len(request.form['passwd1']) >= 0 and (request.form['passwd1'] == request.form['passwd2']):
-			with db.cursor(cursor_factory=psycopg2.extras.DictCursor) as cur:
-				cur.execute('SELECT count(*) as cnt FROM users WHERE login = %s', (request.form['login'],))
-				if cur.fetchall()[0][0] == 0:	
-					print('reg new user\n')				
-					hash = generate_password_hash(request.form['passwd1'])
-					print(f'''{hash}\n{request.form["login"]}''')
-					cur.execute('INSERT INTO users (login, passwd) VALUES (%s, %s);', (request.form['login'], hash))
-					db.commit()
-					return redirect(url_for('login'))
+			if geodb.isFreeLogin(request.form['login']):	
+				print('reg new user\n')				
+				hash = generate_password_hash(request.form['passwd1'])
+				geodb.addUser(request.form['login'], hash)
+				return redirect(url_for('login'))
 	return render_template('register.html')
+
+
+@app.route('/profile')
+@login_required
+def profile():
+	return 
+
+
+@login_manager.user_loader
+def load_user(user_id):
+	print('load user')
+	print(user_id)
+	return UserLogin().fromDB(user_id, geodb)
+
 
 
 @app.route('/upload', methods=['POST'])
@@ -52,7 +78,6 @@ def upload():
 		with open(filename, 'wb') as f:
 			f.write(file.read())
 		db.cursor().execute('INSERT INTO images (name) VALUES (%s)', (hashs,));
-		#db.cursor().execute('INSERT INTO events (description, imgId, userId, shirota, dolgota) VALUES ()') #сделать нормальную схему таблицы, добавить авторизацию, чтобы можно было заполнить юзеров
 		db.commit()
 		cur.close()
 		return redirect(url_for('map', city='moscow'))
@@ -61,17 +86,32 @@ def upload():
 
 
 
-db = None
+geodb = None
 @app.before_request
 def before_request():
-	global db
+	global geodb
 	db = get_db()
+	geodb = GeonetDB(db)
 
 @app.teardown_appcontext
 def close_db(error=None):
-	db = g.pop('db', None)
-	if db is not None:
-		db.close()
+	if hasattr(g, 'db'):
+		g.db.close()
+
+
+
+def get_db():
+	if not hasattr(g, 'db'):
+		g.db = psycopg2.connect(dbname=current_app.config['DATABASE'], user=current_app.config['USER'])
+	return g.db
+
+def create_db():
+    db = psycopg2.connect(user='udk2018', dbname='geonet')
+    with open('schema.sql', 'r') as f:
+        db.cursor().execute(f.read())
+
+
+
 
 
 if __name__ == '__main__':
